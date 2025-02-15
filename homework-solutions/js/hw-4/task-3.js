d09edc712476436a0656fb111935e2a7c060b6d2
Valeria Shut <valeria.shut@gmail.com>
2025-02-15 23:12:48 +0300

    add hw-3 and hw-4

--- homework-solutions/js/hw-4/task-3.js
+++ homework-solutions/js/hw-4/task-3.js
@@ -6,3 +6,29 @@
   преобразовываясь в number
 
 */
+  let minAge = 18;
+  let maxAge = 60;
+  let age = '60';
+  if (age && typeof age !== "number") {
+    age = Number(age);
+    if (age < minAge) {
+      console.log('You don`t have access cause your age is ' + String(age) + ' It`s less then');
+    } 
+    else if (age >= minAge && age < maxAge) {
+      console.log('Welcome  !');
+    } 
+    else if (age > maxAge) {
+      console.log('Keep calm and look Culture channel');
+    } 
+    else console.log('Technical work');
+  }
+  else if (age < minAge) {
+    console.log('You don`t have access cause your age is ' + String(age) + ' It`s less then');
+  } 
+  else if (age >= minAge && age < maxAge) {
+    console.log('Welcome  !');
+  } 
+  else if (age > maxAge) {
+    console.log('Keep calm and look Culture channel');
+  } 
+  else console.log('Technical work');
