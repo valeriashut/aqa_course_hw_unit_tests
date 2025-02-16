12c2306e60edafe8b66f5337c74733c24cffbccf
Valeria Shut <valeria.shut@gmail.com>
2025-02-16 19:51:42 +0300

    changes after review in hw-4 task3

--- homework-solutions/js/hw-4/task-3.js
+++ homework-solutions/js/hw-4/task-3.js
@@ -6,23 +6,21 @@
   преобразовываясь в number
 
 */
-  let minAge = 18;
+let minAge = 18;
   let maxAge = 60;
-  let age = '60';
+  let age = 'ndn';
   if (age && typeof age !== "number") {
     age = Number(age);
-    if (age < minAge) {
+      if (isNaN(age)) {
+        console.log('Incorrect data type');
+      } else if (age < minAge) {
       console.log('You don`t have access cause your age is ' + String(age) + ' It`s less then');
-    } 
-    else if (age >= minAge && age < maxAge) {
+    } else if (age >= minAge && age < maxAge) {
       console.log('Welcome  !');
-    } 
-    else if (age > maxAge) {
+    } else if (age > maxAge) {
       console.log('Keep calm and look Culture channel');
-    } 
-    else console.log('Technical work');
-  }
-  else if (age < minAge) {
+    } else console.log('Technical work');
+  }else if (age < minAge) {
     console.log('You don`t have access cause your age is ' + String(age) + ' It`s less then');
   } 
   else if (age >= minAge && age < maxAge) {
@@ -31,4 +29,4 @@
   else if (age > maxAge) {
     console.log('Keep calm and look Culture channel');
   } 
-  else console.log('Technical work');
+  else console.log('Technical work'); 
