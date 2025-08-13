2073d1fde0de1a9a35183001a7d373ec580ea78f
Valeria Shut <valeria.shut@gmail.com>
2025-08-13 23:54:26 +0300

    HW-7

--- homework-solutions/js/hw-7/task-2.mjs
+++ homework-solutions/js/hw-7/task-2.mjs
@@ -4,8 +4,12 @@
 */
 
 function isPalindrom(word) {
-  // Ваш код
+  if (typeof word !== 'string') return false;
+  const clearWord = word.toLowerCase().replace(/[^a-zа-яё0-9]/g, '');
+  return clearWord === clearWord.split('').reverse().join('');
 }
+console.log(isPalindrom("шиш"));
+
 
 /*
  2. findLongestWords()
@@ -15,7 +19,13 @@ function isPalindrom(word) {
 */
 
 function findLongestWords(sentence) {
-  // Ваш код
+  if (typeof sentence !== 'string') return [];
+  const words = sentence.split(/\s+/);
+  if (words.length === 0 || words[0] === '') return [];
+  const maxLength = Math.max(...words.map(word => word.length));
+  return words.filter(word => word.length === maxLength);
 }
+
+console.log(findLongestWords("Это было самое долгое задание"));
 
 export { isPalindrom, findLongestWords };
