340149463df9a2a78537b3b293dbc02f1e854a0d
Valeria Shut <valeria.shut@gmail.com>
2025-08-24 11:12:07 +0300

    HW-10

--- homework-solutions/js/hw-10/task-2.mjs
+++ homework-solutions/js/hw-10/task-2.mjs
@@ -8,7 +8,13 @@
 */
 
 function countOccurrences(arr) {
-  // ваш код
+return arr.reduce((count, num) => {
+    count[num] = (count[num] || 0) + 1;
+    return count;
+  }, {});
 }
+
+const numbers = [1, 2, 2, 3, 4, 4, 4, 5];
+console.log(countOccurrences(numbers));
 
 export { countOccurrences };
